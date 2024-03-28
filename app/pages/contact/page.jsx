2b6dfa82c77c647a3313c6ccec1c2918e24c1e62
Abk <133124376+Devloperkk@users.noreply.github.com>
2024-03-29 02:55:44 +0530

    a

--- app/pages/contact/page.jsx
+++ app/pages/contact/page.jsx
@@ -13,8 +13,6 @@ export function page() {
 									<div className="single-header">
 										<h3 className="alith_post_title">Contact us</h3>
 									</div>
-                                    <title>Contact Us | Dharmic Dialogue</title>
-                                    <meta name="description" content="Contact Us | Dharmic Dialogue" />
                                     <div className="single-content animate-box">
 
 										<div className="column-1 animate-box">
