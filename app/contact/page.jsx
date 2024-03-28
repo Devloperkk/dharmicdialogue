43bb9f84155c7e5205b93e00c5730d50fe228937
Abk <133124376+Devloperkk@users.noreply.github.com>
2024-03-29 02:12:43 +0530

    contact add

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,4 +1,3 @@
-'use client'
 import React from "react";
 
 export function Contact() {
@@ -58,31 +57,6 @@ export function Contact() {
                 </div>
             </div>
         </div>
-                                        
-
-
-
-
-
-
-
-
-
-      {/* <form onSubmit={handleSubmit}>
-          <div>
-              <label htmlFor="name">Name</label>
-              <input type="text" name="name" required placeholder="Your name" />
-          </div>
-          <div>
-              <label htmlFor="email">Email</label>
-              <input type="email" name="email" required placeholder="email@example.com" />
-          </div>
-          <div>
-              <label htmlFor="message">Message</label>
-              <textarea name="message" required rows="3" placeholder="Enter Message"></textarea>
-          </div>
-          <button type="submit">Submit Form</button>
-      </form> */}
 
     </>
   );
