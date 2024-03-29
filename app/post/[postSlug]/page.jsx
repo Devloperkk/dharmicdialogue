eddfb4a1eb361e3bbe7f2442713dd4498f1f493f
Abk <133124376+Devloperkk@users.noreply.github.com>
2024-03-29 21:57:16 +0530

    Update page.jsx

--- app/post/[postSlug]/page.jsx
+++ app/post/[postSlug]/page.jsx
@@ -21,6 +21,26 @@ function page({params}) {
 
   return (
     <>
+	{postInfo?.map((item,index)=>(
+	<div key={index}>
+<title>{item?.title}</title>
+<meta name="description" content={item?.description} />
+<link href={item?.banner?.url} rel="image_src"/>
+<meta property="og:type" content="article" />
+<meta name="twitter:card" content="summary_large_image" />
+<meta name="twitter:site" content="@dharmicdialogue" />
+<meta name="twitter:creator" content="@dharmicdialogue"/>
+<meta name="twitter:url" content={'https://dharmicdialogue.vercel.app/post/'+item?.slug} />
+<meta name="twitter:title" content={item?.title} />
+<meta name="twitter:description" content={item?.description} />
+<meta name="twitter:image" content={item?.banner?.url} />
+<meta name="twitter:image:src" content={item?.banner?.url} />
+<meta property="og:url" content={'https://dharmicdialogue.vercel.app/post/'+item?.slug} />
+<meta property="og:title" content={item?.title} />
+<meta property="og:description" content={item?.description} />
+<meta property="og:image" content={item?.banner?.url} />
+</div>
+	))}
         <div className="container-fluid">
 			<div className="container">
 				<div className="primary margin-15">
@@ -32,22 +52,7 @@ function page({params}) {
 									<article className="section_margin">
 										{/* meta data */}
 										
-										<title>{item?.title}</title>
-										<meta name="description" content={item?.description} />
-										<link href={item?.banner?.url} rel="image_src"/>
-										<meta property="og:type" content="article" />
-										<meta name="twitter:card" content="summary_large_image" />
-										<meta name="twitter:site" content="@dharmicdialogue" />
-										<meta name="twitter:creator" content="@dharmicdialogue"/>
-										<meta name="twitter:url" content={'https://dharmicdialogue.vercel.app/post/'+item?.slug} />
-										<meta name="twitter:title" content={item?.title} />
-										<meta name="twitter:description" content={item?.description} />
-										<meta name="twitter:image" content={item?.banner?.url} />
-										<meta name="twitter:image:src" content={item?.banner?.url} />
-										<meta property="og:url" content={'https://dharmicdialogue.vercel.app/post/'+item?.slug} />
-										<meta property="og:title" content={item?.title} />
-										<meta property="og:description" content={item?.description} />
-										<meta property="og:image" content={item?.banner?.url} />
+										
 										
 										{/* <div dangerouslySetInnerHTML={{ __html: item?.metaData }}></div> */}
 										
